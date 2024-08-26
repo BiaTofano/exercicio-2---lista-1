c5f559526fb35e5dcbd729186e89aef3022f1b93
BiaTofano <beatriz.t.lemos@gmail.com>
2024-08-26 10:54:59 -0300

    Add files via upload

---
+++ exercício 1.c
@@ -0,0 +1,31 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    int main (){
+
+        int a;
+        int b;
+
+        printf("digite um numero inteiro e positivo");
+        scanf("%d" , &a);
+
+    b = 2;
+
+        while(b < a){
+
+        if (a%b == 0){
+    printf("%d nao e primo", a);
+    break;
+
+    }
+
+    b++;
+
+    }
+
+    if(b == a){
+
+        printf("%d e primo",a);
+    }
+    return 0;
+    }
